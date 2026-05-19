f908f8680f157f1c2bb371712a53ecf133de5d7c
Guddati Laasya Likhita <guddatilaasyalikhita@gmail.com>
2026-05-19 16:17:20 +0530

    Protected dashboard routes with Clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,21 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
+
+});
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!_next|.*\\..*).*)",
+    "/",
+    "/(api|trpc)(.*)",
+  ],
 };
